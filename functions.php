cffdadbbaae2518bbec89a8c418aeba53f545249
CptFlashbang <callum.yates.180@gmail.com>
2024-01-21 17:33:04 +0000

    Added function "add menu"

--- functions.php
+++ functions.php
@@ -3,3 +3,12 @@
         wp_enqueue_style ('style', get_stylesheet_uri());
     }
     add_action ('wp_enqueue_scripts','WildlifeTheme_add_stylesheet');
+
+    function WildlifeTheme_add_menus() {
+        register_nav_menus( array(
+            'primary' => __( 'Primary' ),
+            'secondary' => __( 'Secondary' ),
+        ) );
+    }
+    add_action( 'after_setup_theme',
+                'WildlifeTheme_add_menus' );
